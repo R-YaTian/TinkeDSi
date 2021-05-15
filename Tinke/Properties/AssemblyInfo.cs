76552db7e850bb976a25f53ea83e74443c292917
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2021-05-15 15:35:13 +0800

    Bump version and update AssemblyInfo

--- Tinke/Properties/AssemblyInfo.cs
+++ Tinke/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tinke")]
-[assembly: AssemblyDescription("Viewer and editor for files of NDS games")]
+[assembly: AssemblyTitle("Viewer and editor for files of NDS(i) games")]
+[assembly: AssemblyDescription("Viewer and editor for files of NDS(i) games")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tinke")]
-[assembly: AssemblyCopyright("Copyright © 2012-2016 pleoNeX")]
+[assembly: AssemblyProduct("TinkeDSi")]
+[assembly: AssemblyCopyright("Copyright © 2012-2021 pleoNeX&MetLob&R-YaTian")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1c265b8f-3f75-4fd8-83a6-7b1a2de4dd38")]
-[assembly: AssemblyVersion("0.9.2")]
-[assembly: AssemblyFileVersion("0.9.2")]
+[assembly: AssemblyVersion("0.9.3")]
+[assembly: AssemblyFileVersion("0.9.3")]
